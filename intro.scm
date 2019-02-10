4ec31f3ef1f2bc82bbc3a4fcf7699e658b6c5fcc
Daniel Hines <d4hines@Daniels-MacBook-Pro.local>
2019-02-09 20:25:39 -0500

    on ex 1.30

--- intro.scm
+++ intro.scm
@@ -1,20 +1,25 @@
 (define (dec x)
   (- x 1))
 
+(define (inc x) (+ 1 x))
+
+(define (identity x) x)
+
 (define (square n) (* n n))
 
 ;; Ex. 1.3
 (define sum-2-largest-squares
   (lambda (a b c)
-    (let* ([a>b (> a b)]
-           [b>c (> b c)]
-           [a>c (> a c)]
+    (let* ([a>b (>= a b)]
+           [b>c (>= b c)]
+           [a>c (>= a c)]
            [pair (cond ((and a>b b>c) (list a b))
                        ((and b>c a>c) (list a b))
                        ((and (not a>b) b>c) (list b c))
                        ((and (not b>c) a>c) (list a c))
                        ((and (not a>b) (not b>c)) (list b c))
-                       (else 'error))]
+                       ((and (not a>c) a>b) (list a c)))]
+
            [squared (map square pair)])
       (+ (car squared) (cadr squared)))))
 
@@ -26,29 +31,34 @@
 (sum-2-largest-squares 3 1 2)
 (sum-2-largest-squares 3 2 1)
 
-;; 1.1.7 Square Roots
+;; testing equalities
+(sum-2-largest-squares 2 2 2)
+(sum-2-largest-squares 1 2 2)
+(sum-2-largest-squares 2 1 2)
+(sum-2-largest-squares 2 2 1)
+
+;; ;; 1.1.7 Square Roots
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (improve-sqrt guess x)
-  (average guess (/ x guess)))
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) .0000000001))
-
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve-sqrt
-                   guess x)
-                 x)))
-
 (define (sqrt x)
+  (define (improve-sqrt guess x)
+    (average guess (/ x guess)))
+
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) .0000000001))
+
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve-sqrt
+                    guess x)
+                   x)))
   (sqrt-iter 1.0 x))
 
 (sqrt 2)
 
-;; Ex. 1.8
+;; ;; Ex. 1.8
 (define (cube x) (* x x x))
 
 (define (average3 x y z)
@@ -164,17 +174,10 @@
   (= (remainder n 2)
      0))
 
-(define (fast-expt* b n)
+(define (fast-expt b n)
   (cond ((= n 0) 1)
-        ((even? n) (fast-expt* (square b) (/ n 2)))
-;;        ((even? n) (square (fast-expt* b (/ n 2))))
-        (else (* b (fast-expt* b (dec n))))))
-
-(define (fast-expt* b n a)
-  (cond ((= n 0) 1)
-        ((even? n) (fast-expt* (square b) (/ n 2)))
-        ;;        ((even? n) (square (fast-expt* b (/ n 2))))
-        (else (* b (fast-expt* b (dec n))))))
+        ((even? n) (fast-expt (square b) (/ n 2)))
+        (else (* b (fast-expt b (dec n))))))
 
 (fast-expt 3 0)
 (fast-expt 3 1) 3 24
@@ -199,4 +202,105 @@
 (fast-expt-itr 3 100)
 
 
+;; 1.17
+(define (double n)
+  (* 2 n))
+
+(define (half n)
+  (/ n 2))
+
+(define (fast-multiply a b)
+  (define (fast-multiply* a b)
+    (cond ((or (= a 0) (= b 0)) 0)
+          ((= a 1) b)
+          ((= b 1) a)
+          ((= b 2) (double a))
+          ((= a 2) (double b))
+          ((even? b) (fast-multiply (double a) (half b)))
+          (else (+ a (fast-multiply a (dec b))))))
+  ;; ensure number of recursions doesn't change if a < b
+  (if (< a b)
+      (fast-multiply* b a)
+      (fast-multiply* a b)))
+
+(fast-multiply 10 100)
+
+;; ;; Zero's
+(fast-multiply 0  5)
+(fast-multiply 5  0)
+;; multiplicative identity
+(fast-multiply 1  5)
+(fast-multiply 5  1)
+                                        ; 3 * 4 = 3 + (3 * 3)
+
+; a   b   p
+; 6 * 4 = 24
+; (double 2 6) * (half 4) = 24
+; (doulbe 12) * (half 2) = 24; a = 1
+
+;; 1.30
+
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(sum identity 0 inc 3)
+
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
+(define (product-recursive term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product-recursive term  (next a) next  b))))
+
+(product-recursive identity 1 inc 4)
+
+(product identity 1 inc 4)
+6 10 14
+
+(define (pi accuracy)
+  (define (next n)
+    (/ (* (* 2 n) (+ 2 (* 2 n)))
+       (square (+ (* 2 n) 1))))
+  (* 4.0 (product next 1 inc accuracy)))
+
+(pi 100)
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner
+                            null-value
+                            term
+                            (next a)
+                            next b))))
+
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (if (> a b)
+      null-value
+      (let ([])
+        (combiner (term a)
+                  (accumulate combiner
+                              null-value
+                              term
+                              (next a)
+                              next b)))))
+
+(accumulate + 0 identity 0 inc 3)
+
+(define (sum-higher-order term a next b)
+  (accumulate + 0 term a inc b))
+
+(sum-higher-order identity 0 inc 3)
 
